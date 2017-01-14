52f12b3ce4d57746f312c6efd0e26b15d30824fa
pnlybubbles <pnlybubbles@gmail.com>
2017-01-14 14:53:29 +0900

    feat: migrate to latest grimoirejs

--- index.babel.js
+++ index.babel.js
@@ -1,113 +1,186 @@
-const gf = require('grimoirejs-fundamental').default;
-const {Vector3, AABB} = require('grimoirejs-math').default;
-const {GeometryFactory, GeometryBuilder, GeometryUtility} = gf.Geometry;
+const {Vector3} = require('grimoirejs-math').default;
+const {
+  Geometry,
+  GeometryFactory,
+} = require('grimoirejs-fundamental').default.Geometry;
 
-function* torusIndex(row, column) {
-  for (let i = 0; i < row; i++) {
-    for (let ii = 0; ii < column; ii++) {
-      const r = (column + 1) * i + ii;
-      yield* [r, r + column + 1, r + 1];
-      yield* [r + column + 1, r + column + 2, r + 1];
+class Torus {
+  constructor(row, column, irad, orad) {
+    this.row = row;
+    this.column = column;
+    this.irad = irad;
+    this.orad = orad;
+    this.offset = 0;
+    this.topology = 3;
+    this.positions = [];
+    this.indices = [];
+    this.normals = [];
+    this.texCoord = [];
+    this.count = 0;
+    this.debug = false;
+    this.layout = {
+      POSITION: {
+        size: 3,
+        stride: 32
+      },
+      NORMAL: {
+        size: 3,
+        stride: 32,
+        offset: 12
+      },
+      TEXCOORD: {
+        size: 2,
+        stride: 32,
+        offset: 24
+      },
+    };
+  }
+
+  debugInit() {
+  }
+
+  generate(debug) {
+    if (debug) {
+      this.debug = true;
+      this.debugInit();
+    }
+    this.geo();
+    this.vertices = this.interleave();
+    this.wireframeIndices = this.wireframe();
+    this.count = this.positions.length / this.topology;
+    if (this.debug) {
+      this.validate();
     }
   }
-}
 
-function* torusPosition(row, column, irad, orad) {
-  for (let i = 0; i <= row; i++) {
-    const r = Math.PI * 2 / row * i;
-    const rr = Math.cos(r);
-    const ry = Math.sin(r);
-    for (let ii = 0; ii <= column; ii++) {
-      const tr = Math.PI * 2 / column * ii;
-      const tx = (rr * irad + orad) * Math.cos(tr);
-      const ty = ry * irad;
-      const tz = (rr * irad + orad) * Math.sin(tr);
-      yield* [tx, ty, tz];
+  interleave() {
+    let vertices = [];
+    for (let i = 0; i <= this.positions.length / 3; i++) {
+      vertices.push(...this.positions.slice(i * 3, (i + 1) * 3).concat(this.normals.slice(i * 3, (i + 1) * 3)).concat(this.texCoord.slice(i * 2, (i + 1) * 2)));
+    }
+    return vertices;
+  }
+
+  wireframe() {
+    let indices = [];
+    const ic = new Array(3);
+    for (let i = 0; i <= this.indices.length - 1; i++) {
+      ic[i % 3] = this.indices[i];
+      if (i % 3 === 2) {
+        const a = ic[0], b = ic[1], c = ic[2];
+        indices.push(a, b, b, c, c, a);
+      }
+    }
+    return indices;
+  }
+
+  geo() {
+    for(let i = 0; i <= this.row; i++){
+      const r = Math.PI * 2 / this.row * i;
+      const rr = Math.cos(r);
+      const ry = Math.sin(r);
+      for(let ii = 0; ii <= this.column; ii++){
+        const tr = Math.PI * 2 / this.column * ii;
+        const tx = (rr * this.irad + this.orad) * Math.cos(tr);
+        const ty = ry * this.irad;
+        const tz = (rr * this.irad + this.orad) * Math.sin(tr);
+        const rx = rr * Math.cos(tr);
+        const rz = rr * Math.sin(tr);
+        this.positions.push(tx, ty, tz);
+        this.normals.push(rx, ry, rz);
+        this.texCoord.push(1, 1);
+      }
+    }
+    for(let i_ = 0; i_ < this.row; i_++){
+      for(let ii_ = 0; ii_ < this.column; ii_++){
+        const r = (this.column + 1) * i_ + ii_;
+        this.indices.push(r, r + this.column + 1, r + 1);
+        this.indices.push(r + this.column + 1, r + this.column + 2, r + 1);
+      }
     }
   }
-}
 
-function* torusNormal(row, column) {
-  for (let i = 0; i <= row; i++) {
-    const r = Math.PI * 2 / row * i;
-    const rr = Math.cos(r);
-    const ry = Math.sin(r);
-    for (let ii = 0; ii <= column; ii++) {
-      const tr = Math.PI * 2 / column * ii;
-      var rx = rr * Math.cos(tr);
-      var rz = rr * Math.sin(tr);
-      yield* [rx, ry, rz];
-    }
+  coordToIndex(offset, x, y, up) {
+    return offset + x * (Math.abs(this.div.dotWith(up)) + 1) + y;
   }
-}
 
-function torusSize(row, column) {
-  return (row + 1) * (column + 1);
-}
-
-function torusAABB(irad, orad) {
-  const outerRadius = orad + irad;
-  return new AABB([new Vector3(outerRadius, irad, outerRadius), new Vector3(-outerRadius, -irad, -outerRadius)]);
+  validate() {
+    if (this.positions.length % 3 !== 0) {
+      console.error(`position length(${this.positions.length}) is not a multiple of 3.`);
+    }
+    if (this.normals.length % 3 !== 0) {
+      console.error(`normal length(${this.normals.length}) is not a multiple of 3.`);
+    }
+    if (this.indices.length % this.topology !== 0) {
+      console.error(`index length(${this.indices.length}) is not a multiple of topology(${this.topology}).`);
+    }
+    if (this.texCoord.length % 2 !== 0) {
+      console.error(`texCoord length(${this.texCoord.length}) is not a multiple of 2.`);
+    }
+    if (this.positions.length !== this.normals.length) {
+      console.error(`normal length is not match to position length. normal: ${this.normals.length}, position: ${this.positions.length}`);
+    }
+    if (this.positions.length / 3 !== this.texCoord.length / 2) {
+      console.error(`texCoord pair length is not match to position pair length. texCoord: ${this.texCoord.length / 2}, position: ${this.positions.length / 3}`);
+    }
+    this.indices.forEach((v, idx) => {
+      if (isNaN(parseInt(v)) || parseInt(v) !== v) {
+        console.error(`index(${v}) is not a integer. in: index[${idx}] (${Math.ceil(idx / this.topology)})`);
+      }
+      if (v > this.positions.length / 3) {
+        console.error(`index(${v}) is out of range. in: index[${idx}] (${Math.ceil(idx / this.topology)})`);
+      }
+    });
+    this.positions.forEach((v, idx) => {
+      if (isNaN(parseFloat(v))) {
+        console.error(`position(${v}) is not a number. in: position[${idx}] (${Math.ceil(idx / 3)})`);
+      }
+      if (v > 1.0 || v < -1.0) {
+        console.warn(`position(${v}) is out of unit space(-1 < q < 1). in: position[${idx}] (${Math.ceil(idx / 3)})`);
+      }
+    });
+    Array.from({length: this.normals.length / 3}, (v, i) => {
+      return this.normals.slice(i * 3, i * 3 + 3);
+    }).forEach((v, idx) => {
+      const n = new Vector3(v);
+      if (Math.abs(n.magnitude - 1) > 0.001) {
+        console.warn(`normal(${v.toString()}) is not normalized(${n.magnitude}). in: normal[${idx * 3}..${idx * 3 + 3}] (${idx})`);
+      }
+    });
+    this.texCoord.forEach((v, idx) => {
+      if (isNaN(parseFloat(v))) {
+        console.error(`texCoord(${v}) is not a number. in: texCoord[${idx}] (${Math.ceil(idx / 2)})`);
+      }
+      if (v > 1.0 || v < 0) {
+        console.warn(`texCoord(${v}) is out of unit space(0 < q < 1). in: texCoord[${idx}] (${Math.ceil(idx / 2)})`);
+      }
+    });
+  }
 }
 
 GeometryFactory.addType('torus', {
   row: {
     converter: 'Number',
-    defaultValue: 32,
+    default: 32,
   },
   column: {
     converter: 'Number',
-    defaultValue: 32,
+    default: 32,
   },
   irad: {
     converter: 'Number',
-    defaultValue: 0.5,
+    default: 0.5,
   },
   orad: {
     converter: 'Number',
-    defaultValue: 1.0,
+    default: 1.0,
   },
 }, (gl, attrs) => {
-  return GeometryBuilder.build(gl, {
-    indicies: {
-      default: {
-        generator: function* () {
-          yield* torusIndex(attrs.row, attrs.column);
-        },
-        topology: WebGLRenderingContext.TRIANGLES,
-      },
-      wireframe: {
-        generator: function* () {
-          yield* GeometryUtility.linesFromTriangles(torusIndex(attrs.row, attrs.column));
-        },
-        topology: WebGLRenderingContext.LINES,
-      },
-    },
-    verticies: {
-      main: {
-        size: {
-          position: 3,
-          normal: 3,
-          texCoord: 2,
-        },
-        count: torusSize(attrs.row, attrs.column),
-        getGenerators: () => {
-          return {
-            position: function* () {
-              yield* torusPosition(attrs.row, attrs.column, attrs.irad, attrs.orad);
-            },
-            normal: function* () {
-              yield* torusNormal(attrs.row, attrs.column);
-            },
-            texCoord: function* () {
-              while (true) {
-                yield 1;
-              }
-            },
-          };
-        },
-      },
-    },
-    aabb: torusAABB(attrs.irad, attrs.orad),
-  });
+  const g = new Torus(attrs.row, attrs.column, attrs.irad, attrs.orad);
+  g.generate();
+  const geometry = new Geometry(gl);
+  geometry.addAttributes(g.interleave(), g.layout);
+  geometry.addIndex('default', g.index);
+  geometry.addIndex('wireframe', g.wireframe(), WebGLRenderingContext.LINES);
+  return geometry;
 });
--- index.js
+++ index.js
@@ -1,295 +1,213 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-var _marked = [torusIndex, torusPosition, torusNormal].map(regeneratorRuntime.mark);
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var gf = require('grimoirejs-fundamental').default;
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
-var _require$default = require('grimoirejs-math').default,
-    Vector3 = _require$default.Vector3,
-    AABB = _require$default.AABB;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var _gf$Geometry = gf.Geometry,
-    GeometryFactory = _gf$Geometry.GeometryFactory,
-    GeometryBuilder = _gf$Geometry.GeometryBuilder,
-    GeometryUtility = _gf$Geometry.GeometryUtility;
+var Vector3 = require('grimoirejs-math').default.Vector3;
 
+var _require$default$Geom = require('grimoirejs-fundamental').default.Geometry,
+    Geometry = _require$default$Geom.Geometry,
+    GeometryFactory = _require$default$Geom.GeometryFactory;
 
-function torusIndex(row, column) {
-  var i, ii, r;
-  return regeneratorRuntime.wrap(function torusIndex$(_context) {
-    while (1) {
-      switch (_context.prev = _context.next) {
-        case 0:
-          i = 0;
+var Torus = function () {
+  function Torus(row, column, irad, orad) {
+    _classCallCheck(this, Torus);
 
-        case 1:
-          if (!(i < row)) {
-            _context.next = 13;
-            break;
-          }
+    this.row = row;
+    this.column = column;
+    this.irad = irad;
+    this.orad = orad;
+    this.offset = 0;
+    this.topology = 3;
+    this.positions = [];
+    this.indices = [];
+    this.normals = [];
+    this.texCoord = [];
+    this.count = 0;
+    this.debug = false;
+    this.layout = {
+      POSITION: {
+        size: 3,
+        stride: 32
+      },
+      NORMAL: {
+        size: 3,
+        stride: 32,
+        offset: 12
+      },
+      TEXCOORD: {
+        size: 2,
+        stride: 32,
+        offset: 24
+      }
+    };
+  }
 
-          ii = 0;
-
-        case 3:
-          if (!(ii < column)) {
-            _context.next = 10;
-            break;
-          }
-
-          r = (column + 1) * i + ii;
-          return _context.delegateYield([r, r + column + 1, r + 1], 't0', 6);
-
-        case 6:
-          return _context.delegateYield([r + column + 1, r + column + 2, r + 1], 't1', 7);
-
-        case 7:
-          ii++;
-          _context.next = 3;
-          break;
-
-        case 10:
-          i++;
-          _context.next = 1;
-          break;
-
-        case 13:
-        case 'end':
-          return _context.stop();
+  _createClass(Torus, [{
+    key: 'debugInit',
+    value: function debugInit() {}
+  }, {
+    key: 'generate',
+    value: function generate(debug) {
+      if (debug) {
+        this.debug = true;
+        this.debugInit();
+      }
+      this.geo();
+      this.vertices = this.interleave();
+      this.wireframeIndices = this.wireframe();
+      this.count = this.positions.length / this.topology;
+      if (this.debug) {
+        this.validate();
       }
     }
-  }, _marked[0], this);
-}
-
-function torusPosition(row, column, irad, orad) {
-  var i, r, rr, ry, ii, tr, tx, ty, tz;
-  return regeneratorRuntime.wrap(function torusPosition$(_context2) {
-    while (1) {
-      switch (_context2.prev = _context2.next) {
-        case 0:
-          i = 0;
-
-        case 1:
-          if (!(i <= row)) {
-            _context2.next = 18;
-            break;
-          }
-
-          r = Math.PI * 2 / row * i;
-          rr = Math.cos(r);
-          ry = Math.sin(r);
-          ii = 0;
-
-        case 6:
-          if (!(ii <= column)) {
-            _context2.next = 15;
-            break;
-          }
-
-          tr = Math.PI * 2 / column * ii;
-          tx = (rr * irad + orad) * Math.cos(tr);
-          ty = ry * irad;
-          tz = (rr * irad + orad) * Math.sin(tr);
-          return _context2.delegateYield([tx, ty, tz], 't0', 12);
-
-        case 12:
-          ii++;
-          _context2.next = 6;
-          break;
-
-        case 15:
-          i++;
-          _context2.next = 1;
-          break;
-
-        case 18:
-        case 'end':
-          return _context2.stop();
+  }, {
+    key: 'interleave',
+    value: function interleave() {
+      var vertices = [];
+      for (var i = 0; i <= this.positions.length / 3; i++) {
+        vertices.push.apply(vertices, _toConsumableArray(this.positions.slice(i * 3, (i + 1) * 3).concat(this.normals.slice(i * 3, (i + 1) * 3)).concat(this.texCoord.slice(i * 2, (i + 1) * 2))));
+      }
+      return vertices;
+    }
+  }, {
+    key: 'wireframe',
+    value: function wireframe() {
+      var indices = [];
+      var ic = new Array(3);
+      for (var i = 0; i <= this.indices.length - 1; i++) {
+        ic[i % 3] = this.indices[i];
+        if (i % 3 === 2) {
+          var a = ic[0],
+              b = ic[1],
+              c = ic[2];
+          indices.push(a, b, b, c, c, a);
+        }
+      }
+      return indices;
+    }
+  }, {
+    key: 'geo',
+    value: function geo() {
+      for (var i = 0; i <= this.row; i++) {
+        var r = Math.PI * 2 / this.row * i;
+        var rr = Math.cos(r);
+        var ry = Math.sin(r);
+        for (var ii = 0; ii <= this.column; ii++) {
+          var tr = Math.PI * 2 / this.column * ii;
+          var tx = (rr * this.irad + this.orad) * Math.cos(tr);
+          var ty = ry * this.irad;
+          var tz = (rr * this.irad + this.orad) * Math.sin(tr);
+          var rx = rr * Math.cos(tr);
+          var rz = rr * Math.sin(tr);
+          this.positions.push(tx, ty, tz);
+          this.normals.push(rx, ry, rz);
+          this.texCoord.push(1, 1);
+        }
+      }
+      for (var i_ = 0; i_ < this.row; i_++) {
+        for (var ii_ = 0; ii_ < this.column; ii_++) {
+          var _r = (this.column + 1) * i_ + ii_;
+          this.indices.push(_r, _r + this.column + 1, _r + 1);
+          this.indices.push(_r + this.column + 1, _r + this.column + 2, _r + 1);
+        }
       }
     }
-  }, _marked[1], this);
-}
-
-function torusNormal(row, column) {
-  var i, r, rr, ry, ii, tr, rx, rz;
-  return regeneratorRuntime.wrap(function torusNormal$(_context3) {
-    while (1) {
-      switch (_context3.prev = _context3.next) {
-        case 0:
-          i = 0;
-
-        case 1:
-          if (!(i <= row)) {
-            _context3.next = 17;
-            break;
-          }
-
-          r = Math.PI * 2 / row * i;
-          rr = Math.cos(r);
-          ry = Math.sin(r);
-          ii = 0;
-
-        case 6:
-          if (!(ii <= column)) {
-            _context3.next = 14;
-            break;
-          }
-
-          tr = Math.PI * 2 / column * ii;
-          rx = rr * Math.cos(tr);
-          rz = rr * Math.sin(tr);
-          return _context3.delegateYield([rx, ry, rz], 't0', 11);
-
-        case 11:
-          ii++;
-          _context3.next = 6;
-          break;
-
-        case 14:
-          i++;
-          _context3.next = 1;
-          break;
-
-        case 17:
-        case 'end':
-          return _context3.stop();
-      }
+  }, {
+    key: 'coordToIndex',
+    value: function coordToIndex(offset, x, y, up) {
+      return offset + x * (Math.abs(this.div.dotWith(up)) + 1) + y;
     }
-  }, _marked[2], this);
-}
+  }, {
+    key: 'validate',
+    value: function validate() {
+      var _this = this;
 
-function torusSize(row, column) {
-  return (row + 1) * (column + 1);
-}
+      if (this.positions.length % 3 !== 0) {
+        console.error('position length(' + this.positions.length + ') is not a multiple of 3.');
+      }
+      if (this.normals.length % 3 !== 0) {
+        console.error('normal length(' + this.normals.length + ') is not a multiple of 3.');
+      }
+      if (this.indices.length % this.topology !== 0) {
+        console.error('index length(' + this.indices.length + ') is not a multiple of topology(' + this.topology + ').');
+      }
+      if (this.texCoord.length % 2 !== 0) {
+        console.error('texCoord length(' + this.texCoord.length + ') is not a multiple of 2.');
+      }
+      if (this.positions.length !== this.normals.length) {
+        console.error('normal length is not match to position length. normal: ' + this.normals.length + ', position: ' + this.positions.length);
+      }
+      if (this.positions.length / 3 !== this.texCoord.length / 2) {
+        console.error('texCoord pair length is not match to position pair length. texCoord: ' + this.texCoord.length / 2 + ', position: ' + this.positions.length / 3);
+      }
+      this.indices.forEach(function (v, idx) {
+        if (isNaN(parseInt(v)) || parseInt(v) !== v) {
+          console.error('index(' + v + ') is not a integer. in: index[' + idx + '] (' + Math.ceil(idx / _this.topology) + ')');
+        }
+        if (v > _this.positions.length / 3) {
+          console.error('index(' + v + ') is out of range. in: index[' + idx + '] (' + Math.ceil(idx / _this.topology) + ')');
+        }
+      });
+      this.positions.forEach(function (v, idx) {
+        if (isNaN(parseFloat(v))) {
+          console.error('position(' + v + ') is not a number. in: position[' + idx + '] (' + Math.ceil(idx / 3) + ')');
+        }
+        if (v > 1.0 || v < -1.0) {
+          console.warn('position(' + v + ') is out of unit space(-1 < q < 1). in: position[' + idx + '] (' + Math.ceil(idx / 3) + ')');
+        }
+      });
+      Array.from({ length: this.normals.length / 3 }, function (v, i) {
+        return _this.normals.slice(i * 3, i * 3 + 3);
+      }).forEach(function (v, idx) {
+        var n = new Vector3(v);
+        if (Math.abs(n.magnitude - 1) > 0.001) {
+          console.warn('normal(' + v.toString() + ') is not normalized(' + n.magnitude + '). in: normal[' + idx * 3 + '..' + (idx * 3 + 3) + '] (' + idx + ')');
+        }
+      });
+      this.texCoord.forEach(function (v, idx) {
+        if (isNaN(parseFloat(v))) {
+          console.error('texCoord(' + v + ') is not a number. in: texCoord[' + idx + '] (' + Math.ceil(idx / 2) + ')');
+        }
+        if (v > 1.0 || v < 0) {
+          console.warn('texCoord(' + v + ') is out of unit space(0 < q < 1). in: texCoord[' + idx + '] (' + Math.ceil(idx / 2) + ')');
+        }
+      });
+    }
+  }]);
 
-function torusAABB(irad, orad) {
-  var outerRadius = orad + irad;
-  return new AABB([new Vector3(outerRadius, irad, outerRadius), new Vector3(-outerRadius, -irad, -outerRadius)]);
-}
+  return Torus;
+}();
 
 GeometryFactory.addType('torus', {
   row: {
     converter: 'Number',
-    defaultValue: 32
+    default: 32
   },
   column: {
     converter: 'Number',
-    defaultValue: 32
+    default: 32
   },
   irad: {
     converter: 'Number',
-    defaultValue: 0.5
+    default: 0.5
   },
   orad: {
     converter: 'Number',
-    defaultValue: 1.0
+    default: 1.0
   }
 }, function (gl, attrs) {
-  return GeometryBuilder.build(gl, {
-    indicies: {
-      default: {
-        generator: regeneratorRuntime.mark(function generator() {
-          return regeneratorRuntime.wrap(function generator$(_context4) {
-            while (1) {
-              switch (_context4.prev = _context4.next) {
-                case 0:
-                  return _context4.delegateYield(torusIndex(attrs.row, attrs.column), 't0', 1);
-
-                case 1:
-                case 'end':
-                  return _context4.stop();
-              }
-            }
-          }, generator, this);
-        }),
-        topology: WebGLRenderingContext.TRIANGLES
-      },
-      wireframe: {
-        generator: regeneratorRuntime.mark(function generator() {
-          return regeneratorRuntime.wrap(function generator$(_context5) {
-            while (1) {
-              switch (_context5.prev = _context5.next) {
-                case 0:
-                  return _context5.delegateYield(GeometryUtility.linesFromTriangles(torusIndex(attrs.row, attrs.column)), 't0', 1);
-
-                case 1:
-                case 'end':
-                  return _context5.stop();
-              }
-            }
-          }, generator, this);
-        }),
-        topology: WebGLRenderingContext.LINES
-      }
-    },
-    verticies: {
-      main: {
-        size: {
-          position: 3,
-          normal: 3,
-          texCoord: 2
-        },
-        count: torusSize(attrs.row, attrs.column),
-        getGenerators: function getGenerators() {
-          return {
-            position: regeneratorRuntime.mark(function position() {
-              return regeneratorRuntime.wrap(function position$(_context6) {
-                while (1) {
-                  switch (_context6.prev = _context6.next) {
-                    case 0:
-                      return _context6.delegateYield(torusPosition(attrs.row, attrs.column, attrs.irad, attrs.orad), 't0', 1);
-
-                    case 1:
-                    case 'end':
-                      return _context6.stop();
-                  }
-                }
-              }, position, this);
-            }),
-            normal: regeneratorRuntime.mark(function normal() {
-              return regeneratorRuntime.wrap(function normal$(_context7) {
-                while (1) {
-                  switch (_context7.prev = _context7.next) {
-                    case 0:
-                      return _context7.delegateYield(torusNormal(attrs.row, attrs.column), 't0', 1);
-
-                    case 1:
-                    case 'end':
-                      return _context7.stop();
-                  }
-                }
-              }, normal, this);
-            }),
-            texCoord: regeneratorRuntime.mark(function texCoord() {
-              return regeneratorRuntime.wrap(function texCoord$(_context8) {
-                while (1) {
-                  switch (_context8.prev = _context8.next) {
-                    case 0:
-                      if (!true) {
-                        _context8.next = 5;
-                        break;
-                      }
-
-                      _context8.next = 3;
-                      return 1;
-
-                    case 3:
-                      _context8.next = 0;
-                      break;
-
-                    case 5:
-                    case 'end':
-                      return _context8.stop();
-                  }
-                }
-              }, texCoord, this);
-            })
-          };
-        }
-      }
-    },
-    aabb: torusAABB(attrs.irad, attrs.orad)
-  });
+  var g = new Torus(attrs.row, attrs.column, attrs.irad, attrs.orad);
+  g.generate();
+  var geometry = new Geometry(gl);
+  geometry.addAttributes(g.interleave(), g.layout);
+  geometry.addIndex('default', g.index);
+  geometry.addIndex('wireframe', g.wireframe(), WebGLRenderingContext.LINES);
+  return geometry;
 });
 
 },{"grimoirejs-fundamental":2,"grimoirejs-math":3}],2:[function(require,module,exports){
